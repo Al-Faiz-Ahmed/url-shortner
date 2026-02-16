88537df4de4746e1f8a5b5f86c3efb62a166f52e
Al-Faiz-Ahmed <sheikhfaiz546@gmail.com>
2026-02-17 01:41:15 +0500

    backend config changed

--- backend/src/lib/config/env-config.ts
+++ backend/src/lib/config/env-config.ts
@@ -1,9 +1,9 @@
 import "dotenv/config"
-const { PORT, DATABASE_URL, NODE_ENV, PUBLIC_SITE_URL } = process.env;
+const { PORT, DATABASE_URL, NODE_ENV_STATIC, PUBLIC_SITE_URL } = process.env;
  
 
 export const envConfig = {
-  ENV: NODE_ENV as "development" | "production",
+  ENV: NODE_ENV_STATIC as "development" | "production",
   PORT: parseInt(PORT ?? "5000", 10),
   DATABASE_URL: DATABASE_URL,
   PUBLIC_SITE_URL: PUBLIC_SITE_URL
