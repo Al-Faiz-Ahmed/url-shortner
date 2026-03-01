8e23ee7a9166c3753807ee906d7a98b78d86d75a
Al-Faiz-Ahmed <sheikhfaiz546@gmail.com>
2026-03-01 17:08:04 +0500

    toster added

--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -1,11 +1,14 @@
 import type { ReactNode } from "react";
 import ErrorBoundaryProvider from "./error-bndry-provider";
 import ApolloProviderBase from "./apollo-provider";
-
+import { Toaster } from "@/components/ui/sonner";
 const Providers = ({ children }: { children: Readonly<ReactNode> }) => {
   return (
     <ErrorBoundaryProvider>
-      <ApolloProviderBase>{children}</ApolloProviderBase>
+      <ApolloProviderBase>
+        {children}
+        <Toaster />
+      </ApolloProviderBase>
     </ErrorBoundaryProvider>
   );
 };
