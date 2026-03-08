25e0014412e4bc78c2dc3fe057ca606686f0e3a7
Al-Faiz-Ahmed <sheikhfaiz546@gmail.com>
2026-03-08 07:24:28 +0500

    complete backend cors 2

--- backend/src/middleware/global.ts
+++ backend/src/middleware/global.ts
@@ -11,19 +11,25 @@ const allowedOrigins = [
 export function globalMiddleWareController  (app: Express) {
   
   app.set('trust proxy', 'loopback')
-  app.use(cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
+  // app.use(cors({
+  //   origin: function (origin, callback) {
+  //     // allow requests with no origin (like Postman)
+  //     if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(null, false);
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true
+  //     if (allowedOrigins.includes(origin)) {
+  //       return callback(null, true);
+  //     } else {
+  //       return callback(null, false);
+  //     }
+  //   },
+  //   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  //   credentials: true
+  // }));
+  app.use(cors({
+    origin: allowedOrigins, // Replace with your frontend's actual domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Explicitly allow headers
+    credentials: true // Allow cookies, if needed
   }));
   app.options("*", cors());
   app.use(express.json());
