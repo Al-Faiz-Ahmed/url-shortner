ead657db334eba8d75b9e41b66734b951c02028b
Al-Faiz-Ahmed <sheikhfaiz546@gmail.com>
2026-03-08 07:49:06 +0500

    revert to original

--- backend/src/middleware/global.ts
+++ backend/src/middleware/global.ts
@@ -3,30 +3,14 @@ import express from "express";
 import cors from "cors";
 import ipAddressMiddleware from "./ip-address";
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://tini-tiny.vercel.app"
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://tini-tiny.vercel.app",
+// ];
 
-export function globalMiddleWareController  (app: Express) {
-  
-  app.set('trust proxy', 'loopback')
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        // Allow requests with no origin (e.g. mobile apps, curl, Postman)
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(null,false);
-        }
-      },
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true, // Enable if you use cookies or auth headers
-    })
-  );
-  app.options("*", cors());
+export function globalMiddleWareController(app: Express) {
+  app.set("trust proxy", "loopback");
+  app.use(cors());
   app.use(express.json());
   app.use(ipAddressMiddleware);
 }
