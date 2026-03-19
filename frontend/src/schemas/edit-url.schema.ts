1cdc5cf50912602280b219bc3f758d470218ab95
Al-Faiz-Ahmed <sheikhfaiz546@gmail.com>
2026-03-19 06:22:36 +0500

    frontend: Zod Schema Porblem Fixed

--- frontend/src/schemas/edit-url.schema.ts
+++ frontend/src/schemas/edit-url.schema.ts
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
 export const editUrlSchema = z.object({
-  givenURL: z
-    .string({
-      required_error: "Destination URL is required",
-    })
-    .url("Please type a valid URL"),
+  givenURL: z.url("Please type a valid URL"),
   expirationDate: z.date(),
   isBlock: z.boolean(),
 });
